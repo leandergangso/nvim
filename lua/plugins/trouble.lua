78ed31b863fc26099413e0f78794f340d70d1915
Leander Gangsø <leander.gangso98@gmail.com>
2025-03-12 19:32:22 +0100

    enable trouble

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,24 +1,9 @@
 -- https://github.com/folke/trouble.nvim
 return {
     "folke/trouble.nvim",
-    enabled = false,
+    enabled = true,
     opts = {
         icons = false,
-    },
-    keys = {
-        {
-            "<leander>tt",
-            "<cmd>Trouble diagnostics toggle<cr>",
-            desc = "Diagnostics (Trouble)",
-        },
-        {
-            "<leander>[t",
-            "<cmd>Trouble diagnostics next<cr>",
-        },
-        {
-            "<leander>]t",
-            "<cmd>Trouble diagnostics previous<cr>",
-        },
     },
     config = function()
         local trouble = require("trouble")
