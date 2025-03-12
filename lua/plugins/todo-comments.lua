51d8b2d59b6a4030c78eabbf15181c3173bf0a07
Leander Gangsø <leander.gangso98@gmail.com>
2025-03-12 19:28:27 +0100

    customize comment highlights

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,7 +1,18 @@
 return {
-	"folke/todo-comments.nvim",
-	event = "VimEnter",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	opts = { signs = false },
+    "folke/todo-comments.nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+        signs = false,
+        merge_keywords = true,
+        keywords = {
+            NOTE = { color = "hint", alt = { "INFO" } },
+            TODO = { color = "info" },
+            WARN = { color = "warning", alt = { "HACK" } },
+            FIX = { color = "error", alt = { "BUG", "DEV", "DEL" } },
+            TEST = { color = "test" },
+            PERF = {},
+        }
+    },
 }
 -- TODO: add keymap for telescope picker to list and find existing marks
