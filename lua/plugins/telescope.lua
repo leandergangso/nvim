3980e5b50436c1f1b8fdee90a66cf34aa6761001
Leander Gangsø <leander.gangso98@gmail.com>
2025-03-17 23:41:45 +0100

    upd: telescope open buffer in noraml mode

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,10 @@ return {
         tag = "0.1.8",
         dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
+            local telescope = require("telescope")
+            telescope.setup({
+                pickers = { buffers = { initial_mode = "normal" } }
+            })
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<C-p>", builtin.git_files, {})
             vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
