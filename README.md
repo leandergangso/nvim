cb95478007cc2ef39ace3896566f9a1426773ebf
Leander Gangsø <leander.gangso98@gmail.com>
2025-03-31 00:03:31 +0200

    change keymaps

--- README.md
+++ README.md
@@ -14,3 +14,8 @@
 - Open Neovim with: `nvim`
 - Exec `:Mason` and install whats needed
 
+## Will look into
+
+https://github.com/folke/snacks.nvim
+https://github.com/echasnovski/mini.nvim
+
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,16 +6,19 @@ return {
         config = function()
             local telescope = require("telescope")
             telescope.setup({
-                pickers = { buffers = { initial_mode = "normal" } }
+                pickers = {
+                    buffers = { initial_mode = "normal" },
+                    diagnostics = { initial_mode = "normal" },
+                }
             })
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-            vim.keymap.set("n", "<leader>fw", builtin.grep_string, {})
-            vim.keymap.set("n", "<leader>fd", builtin.diagnostics, {})
-            vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-            vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+            vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>sg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>sw", builtin.grep_string, {})
+            vim.keymap.set("n", "<leader>sd", builtin.diagnostics, {})
+            vim.keymap.set("n", "<leader>sb", builtin.buffers, {})
+            vim.keymap.set("n", "<leader>sh", builtin.help_tags, {})
         end,
     },
     {
