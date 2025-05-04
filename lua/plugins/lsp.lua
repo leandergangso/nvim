8565e92a19ed38b26ad99eb1103ec464ad68f46c
Leander Gangsø <leander.gangso98@gmail.com>
2025-05-04 16:01:45 +0200

    improve diagnostics

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -152,17 +152,23 @@ return {
                 }),
             })
 
-            -- vim.diagnostic.config({
-            --     -- update_in_insert = true,
-            --     float = {
-            --         focusable = false,
-            --         style = "minimal",
-            --         border = "rounded",
-            --         source = "always",
-            --         header = "",
-            --         prefix = "",
-            --     },
-            -- })
+            vim.diagnostic.config({
+                signs = true,
+                underline = true,
+                severity_sort = true,
+                update_in_insert = true,
+                virtual_text = {
+                    spacing = 2,
+                },
+                float = {
+                    focusable = false,
+                    style = "minimal",
+                    border = "rounded",
+                    source = true,
+                    header = "",
+                    prefix = "",
+                },
+            })
         end
     },
 }
