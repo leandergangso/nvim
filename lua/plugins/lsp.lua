a8cc49e8322bc88340a39ca353b52a079e42d36f
Leander Gangsø <leander.gangso98@gmail.com>
2025-06-26 19:32:26 +0200

    chore

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -86,6 +86,15 @@ return {
                                         deprecated = true,
                                         nilness = true,
                                     },
+                                    hints = {
+                                        assignVariableTypes = true,
+                                        compositeLiteralFields = true,
+                                        compositeLiteralTypes = true,
+                                        constantValues = true,
+                                        functionTypeParameters = true,
+                                        parameterNames = true,
+                                        rangeVariableTypes = true,
+                                    },
                                 },
                             },
                         })
