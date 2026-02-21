94055f6ae7a92caaee7c3cd12cb52033bdd39364
Leander Gangsø <leander.gangso98@gmail.com>
2026-02-21 20:07:38 +0100

    chore: which key longer timeout

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,23 +1,23 @@
 return {
 	"folke/which-key.nvim",
+	event = "VimEnter",
 	enabled = true,
 	lazy = true,
-	event = "VimEnter",
 	opts = {
-		delay = 1000,
+		delay = 3000,
 		icons = {
 			mappings = true,
 			keys = {},
 		},
-		spec = {
-			--{ '<leader>',  group = '?' },
-			--{ '<leader>c', group = '[C]ode',     mode = { 'n', 'x' } },
-			--{ '<leader>d', group = '[D]ocument' },
-			--{ '<leader>r', group = '[R]ename' },
-			--{ '<leader>s', group = '[S]earch' },
-			--{ '<leader>w', group = '[W]orkspace' },
-			--{ '<leader>t', group = '[T]oggle' },
-			--{ '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-		},
+		--spec = {
+		--{ '<leader>',  group = '?' },
+		--{ '<leader>c', group = '[C]ode',     mode = { 'n', 'x' } },
+		--{ '<leader>d', group = '[D]ocument' },
+		--{ '<leader>r', group = '[R]ename' },
+		--{ '<leader>s', group = '[S]earch' },
+		--{ '<leader>w', group = '[W]orkspace' },
+		--{ '<leader>t', group = '[T]oggle' },
+		--{ '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+		--},
 	},
 }
