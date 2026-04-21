80f5ad58f35790a0f8e8a3ed545713d957b95193
Leander Gangsø <leander.gangso98@gmail.com>
2026-04-21 20:23:54 +0200

    chore: update treesitter
    nvim-treesitter is now archived, nvim v0.12 has moved majority of the logic from nvim-treesitter directly into nvim

--- init.lua
+++ init.lua
@@ -1,7 +1,9 @@
 require("config.opts")
 require("config.keymaps")
+
+require("config.lazy")
+
 require("config.snippets")
 require("config.filetypes")
 require("config.events")
-
-require("config.lazy")
+require("config.treesitter")
---
+++ lua/config/treesitter.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+M.parsers = {
+	"lua",
+	"html",
+	"css",
+	"vimdoc",
+	"jsdoc",
+	"bash",
+	"go",
+	"astro",
+	"svelte",
+	"python",
+	"javascript",
+	"typescript",
+}
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = M.parsers,
+	callback = function()
+		vim.treesitter.start()
+	end,
+})
+
+return M
