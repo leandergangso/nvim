4a60dc03462a111980502494b231851b7db7e331
Leander Gangso <leander.gangso98@gmail.com>
2026-05-11 11:06:10 +0200

    chore

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -14,8 +14,8 @@ vim.opt.scrolloff = 10
 vim.opt.showmode = false
 
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
