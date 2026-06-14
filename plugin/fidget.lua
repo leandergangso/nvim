73c687af1cc9840d892c42f7a604b9e2bccb22ad
Leander Gangsø <leander.gangso98@gmail.com>
2026-06-14 02:22:04 +0200

    chore: plugin and pack-lock

--- plugin/fidget.lua
+++ plugin/fidget.lua
@@ -1,11 +1,5 @@
-if true then
-	return
-end
-
--- NOTE: disabled, testing if i can remove fully
-
-vim.pack.add({
-	"github.com/j-hui/fidget.nvim",
-})
-
-require("fidget").setup({})
+--vim.pack.add({
+--	"github.com/j-hui/fidget.nvim",
+--})
+--
+--require("fidget").setup({})
