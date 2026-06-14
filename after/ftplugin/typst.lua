eec724340000f493decf7e39849e5c0aaaf54c82
Leander Gangsø <leander.gangso98@gmail.com>
2026-06-14 02:48:27 +0200

    chore

--- after/ftplugin/typst.lua
+++ after/ftplugin/typst.lua
@@ -6,8 +6,7 @@ vim.pack.add({
 	"github.com/chomosuke/typst-preview.nvim",
 })
 
-local typst_preview = require("typst-preview")
-typst_preview.setup({
+require("typst-preview").setup({
 	follow_cursor = true,
 	dependencies_bin = {
 		tinymist = vim.fn.exepath("tinymist"),
